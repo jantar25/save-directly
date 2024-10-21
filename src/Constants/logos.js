0604f6343d10d55ee08eead38b290cc9e4e63a55
Gloire_25 <gloire.bulambo@grandlies.com>
2024-10-21 21:17:52 +0200

    added logo image

--- src/Constants/logos.js
+++ src/Constants/logos.js
@@ -1,9 +1,11 @@
 import danubeLogo from "../Assets/Images/danube.png";
 import radiantLogo from "../Assets/Images/radiant.png";
+import motionsLogo from "../Assets/Images/moshions.png";
 
 const marchantLogos = {
   'RADIANT': radiantLogo,
   'DANUBE HOME': danubeLogo,
+  'MOSHIONS': motionsLogo
 };
 
 export default marchantLogos;
